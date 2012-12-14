da8ad9e29d8c9728fc3d6d7723db64783ca26144
Masahiro Ihara <ihara@cookpad.com>
2012-12-14 13:07:18 +0900

    Support android tablet

--- lib/rack/smartphone_detector.rb
+++ lib/rack/smartphone_detector.rb
@@ -6,15 +6,24 @@ require 'rack/smartphone_detector/railtie' if defined?(Rails::Railtie)
 
 module Rack
   class SmartphoneDetector
-    SMARTPHONES_REGEXP = /(iPhone|iPad|Android|Windows Phone)/
+    SMARTPHONE_IDENTIFIERS = [
+      { identifier: 'iPhone',         regexp: /iPhone/ },
+      { identifier: 'iPad',           regexp: /iPad/ },
+      { identifier: 'Android',        regexp: /Android.+Mobile/ },
+      { identifier: 'Android Tablet', regexp: /Android/ },
+      { identifier: 'Windows Phone',  regexp: /Windows Phone/ },
+    ]
 
     def initialize(app, options = {})
       @app = app
     end
 
     def call(env)
-      if env['HTTP_USER_AGENT'] =~ SMARTPHONES_REGEXP
-        env['rack.smartphone_detector.device'] = $1
+      SMARTPHONE_IDENTIFIERS.each do |i|
+        if env['HTTP_USER_AGENT'] =~ i[:regexp]
+          env['rack.smartphone_detector.device'] = i[:identifier]
+          break
+        end
       end
       @app.call(env)
     end
--- spec/rack-smartphone_detector_spec.rb
+++ spec/rack-smartphone_detector_spec.rb
@@ -9,7 +9,8 @@ describe 'Rack::SmartphoneDetector' do
   SMARTPHONES = [
     { device: 'iPhone', user_agent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25' },
     { device: 'iPad', user_agent: 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25' },
-    { device: 'Android', user_agent: 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03S) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19' },
+    { device: 'Android', user_agent: 'Mozilla/5.0 (Linux; U; Android 4.0.1; ja-jp; Galaxy Nexus Build/ITL41D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' },
+    { device: 'Android Tablet', user_agent: 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03S) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19' },
     { device: 'Windows Phone', user_agent: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; FujitsuToshibaMobileCommun; IS12T; KDDI)' }
   ]
 
